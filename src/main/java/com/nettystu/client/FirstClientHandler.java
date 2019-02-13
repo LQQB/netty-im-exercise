42ce9f31926b549d1292e6d36b7dd849e3761c12
LQQB <897680736@qq.com>
2019-02-13 17:52:08 +0800

    服务端向客户端发送数据

--- src/main/java/com/nettystu/client/FirstClientHandler.java
+++ src/main/java/com/nettystu/client/FirstClientHandler.java
@@ -23,12 +23,21 @@ public class FirstClientHandler extends ChannelInboundHandlerAdapter {
         ctx.channel().writeAndFlush(byteBuf);
     }
 
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg;
+
+        System.out.println(new Date() + "客户端读取到数据 ->" + byteBuf.toString(Charset.forName("utf-8")));
+
+    }
+
     private ByteBuf getByteBuf(ChannelHandlerContext ctx) {
         // 1. 获取二进制抽象 bytebuf
+        // ctx.alloc() 获得一个 ByteBuf 的内存管理器，这个内存管理器的作用是分配一个 ByteBBuf
         ByteBuf buf = ctx.alloc().buffer();
 
         // 准备数据，并指定字符集为 UTF-8
-        byte[] bytes = "您好LQB".getBytes(Charset.forName("UTF-8"));
+        byte[] bytes = "您好我是LQB的客户端".getBytes(Charset.forName("UTF-8"));
 
         // 3. 填充数据到 ByteBuf
         buf.writeBytes(bytes);
--- src/main/java/com/nettystu/server/FirstServerHandler.java
+++ src/main/java/com/nettystu/server/FirstServerHandler.java
@@ -21,6 +21,21 @@ public class FirstServerHandler extends ChannelInboundHandlerAdapter {
 
         System.out.println(new Date() + "服务端读取到数据 ->" + byteBuf.toString(Charset.forName("utf-8")));
 
+
+        // 回复数据到客户端
+        System.out.println(new Date() + "服务端写出数据");
+        ByteBuf out = getByteBuf(ctx);
+        ctx.channel().writeAndFlush(out);
+    }
+
+    private ByteBuf getByteBuf(ChannelHandlerContext ctx) {
+        byte[] bytes = "您好我是LQQB的服务端".getBytes(Charset.forName("UTF-8"));
+
+        ByteBuf byteBuf = ctx.alloc().buffer();
+
+        byteBuf.writeBytes(bytes);
+
+        return byteBuf;
     }
 
 
