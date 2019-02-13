73a38d19e06a2e4b9387d1e79e841a3d5e1e1687
LQQB <897680736@qq.com>
2019-02-13 14:01:07 +0800

    服务端启动流程

--- src/main/java/com/nettystu/NettyServer.java
+++ src/main/java/com/nettystu/NettyServer.java
@@ -6,6 +6,8 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 public class NettyServer {
 
@@ -17,7 +19,12 @@ public class NettyServer {
         ServerBootstrap serverBootstrap = new ServerBootstrap();
         serverBootstrap
                 .group(boassGroup, workerGroup)
+                // .channel() 来指定 IO 模型(NioServerSocketChannel.class 是 Nio 模型 , OioServerSocketChannel.class 是 BIO 模型
                 .channel(NioServerSocketChannel.class)
+                // childHandler() 方法 给引导类创建一个 ChannelInitializer,主要用来定义后续每条连接的数据读写，业务处理逻辑
+                // ChannelInitializer 有一个泛型参数 NioSocketChannel，这个类 就是 Netty 对 NIO 类型的连接抽象。
+                // 前面用到的 NioServerSocketChannel 也是对 NIO连接的抽象， NioServerSocketChannel 和 NioSocketChannel 的概念可以跟
+                // ServerSocket 以及 Socket 两个概念对应
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
@@ -25,7 +32,18 @@ public class NettyServer {
                     }
                 });
 
-        serverBootstrap.bind(8000);
+        // .bind() 是一个异步的方法，它一调用就会立即返回一个 ChannelFuture，给 ChannelFuture 添加监听器 GenericFutureListener 然后我们
+        // 就可以在 GenericFutureListener 的 operationComplete 方法里面，监听端口是否绑定成功
+        serverBootstrap.bind(1000).addListeners(new GenericFutureListener<Future<? super Void>>() {
+            @Override
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if(future.isSuccess()) {
+                    System.out.println("端口绑定成功");
+                } else {
+                    System.err.println("端口绑定失败");
+                }
+            }
+        });
 
     }
 
