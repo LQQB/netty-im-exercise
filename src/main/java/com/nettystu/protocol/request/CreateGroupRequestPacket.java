a65d13aa53155840a04b33f0874df4e79eb2b05d
LQQB <897680736@qq.com>
2019-02-18 23:23:40 +0800

    创建群聊请求响应数据包

--- src/main/java/com/nettystu/protocol/request/CreateGroupRequestPacket.java
+++ src/main/java/com/nettystu/protocol/request/CreateGroupRequestPacket.java
@@ -1,10 +1,16 @@
 package com.nettystu.protocol.request;
 
 import com.nettystu.protocol.Packet;
+import lombok.Data;
+
+import java.util.List;
 
 import static com.nettystu.protocol.command.Command.CREATE_GROUP_REQUEST;
 
+@Data
 public class CreateGroupRequestPacket extends Packet {
+
+    private List<String> userIdList;
 
 
     @Override
